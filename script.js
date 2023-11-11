cb78cf5560282ba4d0aa00bdc39000f71eebcf61
ssprasad-cyber <2040320ssprasad990@gmail.com>
2023-11-11 22:36:22 +0530

    code updates

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
 const btn = document.querySelector(".menu");
 const link = document.querySelector(".links");
 const nav = document.querySelector("nav");
+const nut = document.querySelector(".nul");
+const main = document.querySelector(".main");
+const page1 = document.querySelector(".page1");
 btn.addEventListener("click",() => {
     link.classList.toggle("mobile-menu");
     nav.classList.toggle("nav-li");
+    nut.classList.toggle("null-a");
+    main.classList.toggle("null-a");
+    page1.classList.toggle("null-a");
 })
-
+console.log(nut)
 //notification bar;
 const notify = document.querySelector(".notifications");
 const notifications = new Set(["eager club batch 1 -20 members", "eager club batch 2 -34 members", "eager club batch 3 -102 members"]);
